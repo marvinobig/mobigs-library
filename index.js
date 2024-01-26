2ca35cb58382e0878ccb23b32818d5c18937023c
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-01-26 18:25:46 +0000

    use constructor to create book object

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-console.log("MObig's Library")
-
 function bookConstructor(bookName, bookDescription, read) {
     this.bookName = bookName;
     this.bookDescription = bookDescription;
     this.read = read;
 }
 
+console.log("MObig's Library");
+
 const book = new bookConstructor('ATMYEN', 'Self teaching guide to relaern secondary school math', false);
 
 console.log(book);
+
+const div = document.createElement('div');
+const para1 = document.createElement('p');
+const para2 = document.createElement('p');
+const para3 = document.createElement('p');
+para1.textContent = book.bookName;
+para2.textContent = book.bookDescription;
+para3.textContent = book.read;
+
+div.append(para1, para2, para3);
+document.body.append(div);
