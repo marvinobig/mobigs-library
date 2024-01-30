bd287fb97c8fa5c683fdcce9e8c7821207dbab8a
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-01-30 17:10:14 +0000

    build out 'add book' functionality

--- index.js
+++ index.js
@@ -1,23 +1,73 @@
-function BookConstructor(bookName, bookDescription, read) {
-    this.bookName = bookName;
-    this.bookDescription = bookDescription;
-    this.read = read;
+const bookStorage = [];
+let currID = 0;
+const libraryElement = document.querySelector("#library-display");
+const bookFormBtn = document.querySelector("#book-form-btn");
+const bookFormSubmitBtn = document.querySelector("#book-submit");
+const closeBtn = document.querySelector("#close-btn");
+const bookForm = document.querySelector("#book-form-modal");
+
+function BookConstructor(bookID, bookName, bookDescription, pages, read) {
+  this.bookID = bookID;
+  this.bookName = bookName;
+  this.bookDescription = bookDescription;
+  this.pages = pages;
+  this.read = read;
 }
 
-const bookStorage = [];
-
 // Create and store book
-const book = new BookConstructor('ATMYEN', 'Self teaching guide to relearn secondary school math', false);
-bookStorage.push(book);
+function storeBook(BookConstructor, bookStorage, currID) {
+  const bookTitle = document.querySelector("#book-name").value;
+  const bookDescription = document.querySelector("#book-description").value;
+  const bookPages = document.querySelector("#book-pages").value;
+  const readStatus = document.querySelector("#read-status").value;
 
-// Creating book in DOM using constructor function
-const div = document.createElement('div');
-const para1 = document.createElement('p');
-const para2 = document.createElement('p');
-const para3 = document.createElement('p');
-para1.textContent = book.bookName;
-para2.textContent = book.bookDescription;
-para3.textContent = book.read;
+  const book = new BookConstructor(
+    currID,
+    bookTitle,
+    bookDescription,
+    bookPages,
+    readStatus
+  );
 
-div.append(para1, para2, para3);
-document.body.append(div);
+  bookStorage.push(book);
+  console.log(bookStorage);
+}
+
+function displayBook(libraryElement, book) {
+  const bookCard = document.createElement("article");
+  const title = document.createElement("h3");
+  const para2 = document.createElement("p");
+  const para3 = document.createElement("p");
+  const para4 = document.createElement("p");
+  title.textContent = book.bookName;
+  para2.textContent = book.bookDescription;
+  para3.textContent = book.pages;
+  para4.textContent = book.read;
+
+  bookCard.append(title, para2, para3);
+  libraryElement.append(bookCard);
+}
+
+bookFormBtn.addEventListener("click", () => {
+  bookForm.showModal();
+});
+
+closeBtn.addEventListener("click", () => {
+  bookForm.close();
+});
+
+bookFormSubmitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  storeBook(BookConstructor, bookStorage, currID);
+  bookForm.close();
+  
+  // Creating book in DOM using constructor function
+  bookStorage.forEach((book) => {
+    if (book.bookID == currID) {
+      displayBook(libraryElement, book);
+    }
+  });
+  
+  currID++;
+});
