d094605548171de1a1b3ffccaaded287a2633c4c
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-01-31 18:03:28 +0000

    implement 'delete book' and 'read status' functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const bookStorage = [];
 let currID = 0;
 const libraryElement = document.querySelector("#library-display");
+const resetBtn = document.querySelector("#reset-btn");
 const bookFormBtn = document.querySelector("#book-form-btn");
 const bookFormSubmitBtn = document.querySelector("#book-submit");
 const closeBtn = document.querySelector("#close-btn");
@@ -19,7 +20,7 @@ function storeBook(BookConstructor, bookStorage, currID) {
   const bookTitle = document.querySelector("#book-name").value;
   const bookDescription = document.querySelector("#book-description").value;
   const bookPages = document.querySelector("#book-pages").value;
-  const readStatus = document.querySelector("#read-status").value;
+  const readStatus = document.querySelector("#read-status").checked;
 
   const book = new BookConstructor(
     currID,
@@ -30,22 +31,56 @@ function storeBook(BookConstructor, bookStorage, currID) {
   );
 
   bookStorage.push(book);
-  console.log(bookStorage);
 }
 
-function displayBook(libraryElement, book) {
+function displayBook(libraryElement, book, bookStorage) {
   const bookCard = document.createElement("article");
   const title = document.createElement("h3");
   const para2 = document.createElement("p");
   const para3 = document.createElement("p");
-  const para4 = document.createElement("p");
+  const readBtn = document.createElement("button");
+  const deleteBtn = document.createElement("button");
+
+  deleteBtn.setAttribute("id", "delete-btn");
+  deleteBtn.textContent = "X";
+  deleteBtn.value = book.bookID;
   title.textContent = book.bookName;
   para2.textContent = book.bookDescription;
   para3.textContent = book.pages;
-  para4.textContent = book.read;
+  readBtn.setAttribute("id", `read-btn-${book.bookID}`);
+  readBtn.textContent = book.read ? "Read" : "Not Read";
+  readBtn.value = book.bookID;
 
-  bookCard.append(title, para2, para3);
+  bookCard.append(deleteBtn, title, para2, para3, readBtn);
   libraryElement.append(bookCard);
+
+  deleteBtn.addEventListener("click", (e) =>
+    deleteBook(bookCard, bookStorage, Number(e.target.value))
+  );
+  readBtn.addEventListener("click", (e) =>
+    setReadStatus(readBtn, bookStorage, Number(e.target.value))
+  );
+}
+
+function deleteBook(cardElement, bookStorage, id) {
+  const newBookArr = bookStorage.filter((book) => book.bookID !== id);
+
+  bookStorage.splice(0, bookStorage.length, ...newBookArr);
+  cardElement.remove();
+}
+
+function setReadStatus(btn, bookStorage, id) {
+  bookStorage.forEach((book) => {
+    if (book.bookID === id) {
+      if (book.read) {
+        book.read = false;
+        btn.textContent = "Not Read";
+      } else {
+        book.read = true;
+        btn.textContent = "Read";
+      }
+    }
+  });
 }
 
 bookFormBtn.addEventListener("click", () => {
@@ -61,13 +96,13 @@ bookFormSubmitBtn.addEventListener("click", (e) => {
 
   storeBook(BookConstructor, bookStorage, currID);
   bookForm.close();
-  
+
   // Creating book in DOM using constructor function
   bookStorage.forEach((book) => {
     if (book.bookID == currID) {
-      displayBook(libraryElement, book);
+      displayBook(libraryElement, book, bookStorage);
     }
   });
-  
+
   currID++;
 });
